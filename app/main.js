20815c358429100320520154ed0aa381ca6f736e
thewibblywobbly <georgemcdaid@gmail.com>
2016-07-11 23:56:19 -0400

    added crm components and gulp packages

--- app/main.js
+++ app/main.js
@@ -10,13 +10,13 @@ let win;
 
 function createWindow() {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600});
+    win = new BrowserWindow({width: 1400, height: 900});
 
     // and load the index.html of the app.
     win.loadURL(`file://${__dirname}/index.html`);
 
     // Open the DevTools.
-    win.webContents.openDevTools();
+    //win.webContents.openDevTools();
 
     // Emitted when the window is closed.
     win.on('closed', () => {
